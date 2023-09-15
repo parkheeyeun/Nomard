c634c7157ab77e47530cdd22797192df6adfb3af
Cheki <bom0704@naver.com>
2023-09-15 17:51:05 +0900

    Add files via upload

---
+++ financial/lib/widgets/CurrencyCard.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class CurrencyCard extends StatelessWidget {
+  final String name, code, amount;
+  final IconData icon;
+  final bool isInverted;
+  final int order; 
+
+  final blackcolor = const Color(0xFF1F2123);
+
+  const CurrencyCard({
+    super.key,
+    required this.name,
+    required this.code,
+    required this.amount,
+    required this.icon,
+    required this.isInverted,
+    required this.order,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(0, order * -10),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: isInverted ? const Color(0xFFFAFAFA) : blackcolor,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(
+            25,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: isInverted ? blackcolor : Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        amount,
+                        style: TextStyle(
+                          color: isInverted ? blackcolor : Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        code,
+                        style: TextStyle(
+                          color: isInverted
+                              ? blackcolor
+                              : Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Transform.scale(
+                scale: 2,
+                child: Transform.translate(
+                  offset: const Offset(-5, 18),
+                  child: Icon(
+                    icon,
+                    color: isInverted ? blackcolor : Colors.white,
+                    size: 88,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+}
